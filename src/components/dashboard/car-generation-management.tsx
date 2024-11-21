9d0f728a65759a4f2d766a5776d0925feb1494a0
Froggy-san <medoosama441@yahoo.com>
2024-11-21 03:13:10 +0200

    Trying to redeploy to varcel

--- src/components/dashboard/car-generation-management.tsx
+++ src/components/dashboard/car-generation-management.tsx
@@ -20,6 +20,7 @@ const CarGenAndModelManagement = async () => {
 
   return (
     <>
+      <h1>TESTIMGG</h1>
       <div className="flex  flex-col  gap-y-2 xs:flex-row xs:items-center justify-between rounded-lg border p-3 shadow-sm gap-x-7">
         <div className="space-y-0.5   ">
           <label className=" font-semibold">Car models</label>
