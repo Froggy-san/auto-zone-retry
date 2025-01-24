0d54ca09079ad2d28d412a437f18422aed15805d
Froggy-san <medoosama441@yahoo.com>
2025-01-24 22:08:44 +0200

    Added stripe to the app

---
+++ src/app/stripe/page.tsx
@@ -0,0 +1,12 @@
+import Stripe from "@components/stripe/stripe";
+import React from "react";
+
+const Page = () => {
+  return (
+    <main>
+      <Stripe />
+    </main>
+  );
+};
+
+export default Page;
