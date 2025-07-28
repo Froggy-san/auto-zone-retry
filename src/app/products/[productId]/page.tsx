76429aee9fca10c4bd42876f676ad005e5da7646
Froggy-san <medoosama441@yahoo.com>
2025-07-28 04:46:28 +0300

    Added some more product naviations on mobile phones to the products page.

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -13,6 +13,7 @@ import { getAllProductBrandsAction } from "@lib/actions/productBrandsActions";
 import { getAllProductTypesAction } from "@lib/actions/productTypeActions";
 import DeleteManagement from "@components/products/delete-management";
 import { Metadata } from "next";
+import { ProductSwipeNavigator } from "@components/products/product-swipe-navigator";
 
 export const metadata: Metadata = {
   title: "Product Details",
@@ -73,8 +74,13 @@ const ProductView = async ({
     );
 
   return (
-    <div className=" relative">
-      {nextPro && (
+    <ProductSwipeNavigator
+      currentProductId={productData.id}
+      prevProductId={prevPro}
+      nextProductId={nextPro}
+    >
+      <div className=" relative">
+        {/* {nextPro && (
         <div className=" fixed -left-10 top-0  hover:left-0 z-30  hover:backdrop-blur-sm  transition-all  h-full w-16 flex items-center justify-center">
           <Button asChild variant="secondary">
             <Link href={`/products/${nextPro}`} replace>
@@ -92,45 +98,46 @@ const ProductView = async ({
             </Link>
           </Button>
         </div>
-      )}
-      {imageUrls?.length ? (
-        <FullImagesGallery images={imageUrls} productId={productData.id} />
-      ) : (
-        <div className=" h-full flex items-center justify-center  bg-foreground/10  font-semibold text-xl py-5 gap-3">
-          <ImageOff className=" w-10 h-10" /> No images.
-        </div>
-      )}
-      <div className=" text-xs  text-muted-foreground my-4 text-right px-3">
-        {productData.stock ? (
-          <i>
-            Stock: <span>{productData.stock}</span>
-          </i>
+      )} */}
+        {imageUrls?.length ? (
+          <FullImagesGallery images={imageUrls} productId={productData.id} />
         ) : (
-          <i>Out of stock</i>
+          <div className=" h-full flex items-center justify-center  bg-foreground/10  font-semibold text-xl py-5 gap-3">
+            <ImageOff className=" w-10 h-10" /> No images.
+          </div>
         )}
-      </div>
-      <ProdcutViewDetials user={user} product={productData} />
-
-      {user ? (
-        <div className=" flex flex-col mb-5 sm:flex-row items-center gap-5  px-2  sm:px-5">
-          <ProductManagement
-            useParams
-            className=" w-full"
-            categories={categoriesData}
-            productBrands={productBrandsData}
-            productTypes={brandTypesData}
-            productToEdit={productData}
-          />
-
-          <DeleteManagement
-            imagesToDelete={imageUrls}
-            pageSize={Number(pageSize)}
-            currPage={Number(currPage)}
-            productId={Number(params.productId)}
-          />
+        <div className=" text-xs  text-muted-foreground my-4 text-right px-3">
+          {productData.stock ? (
+            <i>
+              Stock: <span>{productData.stock}</span>
+            </i>
+          ) : (
+            <i>Out of stock</i>
+          )}
         </div>
-      ) : null}
-    </div>
+        <ProdcutViewDetials user={user} product={productData} />
+
+        {user ? (
+          <div className=" flex flex-col mb-5 sm:flex-row items-center gap-5  px-2  sm:px-5">
+            <ProductManagement
+              useParams
+              className=" w-full"
+              categories={categoriesData}
+              productBrands={productBrandsData}
+              productTypes={brandTypesData}
+              productToEdit={productData}
+            />
+
+            <DeleteManagement
+              imagesToDelete={imageUrls}
+              pageSize={Number(pageSize)}
+              currPage={Number(currPage)}
+              productId={Number(params.productId)}
+            />
+          </div>
+        ) : null}
+      </div>
+    </ProductSwipeNavigator>
   );
 };
 
